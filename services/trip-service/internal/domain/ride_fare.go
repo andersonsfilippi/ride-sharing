83c77c7ea7afc6b76caf155038bf96f897086659
Anderson Filippi <andersonfilippi@gmail.com>
2026-06-09 23:34:06 -0300

    impl trip fare generation

--- services/trip-service/internal/domain/ride_fare.go
+++ services/trip-service/internal/domain/ride_fare.go
@@ -3,6 +3,7 @@ package domain
 import (
 	"time"
 
+	pb "github.com/andersonsfilippi/ride-sharing/shared/proto/trip"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
@@ -12,4 +13,22 @@ type RideFareModel struct {
 	PackageSlug       string  // ex: van, luxury, sedan
 	TotalPriceInCents float64 // calculated price of price
 	ExpiresAt         time.Time
+}
+
+func (r *RideFareModel) ToProto() *pb.RideFare {
+	return &pb.RideFare{
+		Id:                r.ID.Hex(),
+		UserID:            r.UserID,
+		PackageSlug:       r.PackageSlug,
+		TotalPriceInCents: r.TotalPriceInCents,
+	}
+}
+
+func ToRideFaresProto(fares []*RideFareModel) []*pb.RideFare {
+	var protoFares []*pb.RideFare
+
+	for _, f := range fares {
+		protoFares = append(protoFares, f.ToProto())
+	}
+	return protoFares
 }
